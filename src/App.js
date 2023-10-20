ab84993daf19eacbfdf0ee2288ce815f122e0d1d
andleebrehman <120009252+andleebrehman@users.noreply.github.com>
2023-10-20 11:30:55 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LoginForm.css"; // Import your CSS file
+import LoginForm from "./LoginForm";
+
+function App() {
+  return (
+    <div className="App" style={{ backgroundColor: "#F6F7FB" }}>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
